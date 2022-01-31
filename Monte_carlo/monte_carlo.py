9fca08995b4a10cf7dac2692e3cafb8c9757a52a
Roo541 <33165603+Roo541@users.noreply.github.com>
2022-01-31 10:27:29 -0700

    Update monte_carlo.py

--- Monte_carlo/monte_carlo.py
+++ Monte_carlo/monte_carlo.py
@@ -3,26 +3,31 @@ import pandas as pd
 import strategy_creator as strategy
 import random as rand 
 import backtest 
+import datetime as dt
+import math
 
 #define length of simulation
-start = '2019-12-31'
-end = '2020-12-31'
-
+start = '2015-12-31'
+end = '2016-12-31'
+start_time = dt.datetime.now()
 returns = {'iteration':[], 'return':[], 'p1':[],'p2':[] ,'p3':[], 'p4':[], 'p5':[]}
 #make strategy 
-for i in range(100):
-    print(i)
-    if i == 0:
-        p1=p2=p3=p4=p5= 1
-    else:
-        p1 = i/100
-        p2 = 0.5
-        p3 = 0.5
-        p4 = 0.5
-        p5 = 0.5
+for i in range(500):
+    v1 = rand.randint(-100,100)
+    v2 = rand.randint(-100,100)
+    v3 = rand.randint(-100,100)
+    v4 = rand.randint(-100,100)
+    v5 = rand.randint(-100,100)
+
+    mag = math.sqrt(v1*v1+v2*v2+v3*v3+v4*v4+v5*v5)
+
+    p1 = (v1/mag)
+    p2 = (v2/mag)
+    p3 = (v3/mag)
+    p4 = (v4/mag)
+    p5 = (v5/mag) 
 
     strategy.strategy(p1, p2, p3, p4, p5, start, end)
-    print('done')
     alpha = backtest.backtesting(start, end, i)
 
     returns['iteration'].append(i)
@@ -33,7 +38,9 @@ for i in range(100):
     returns['p4'].append(p4)
     returns['p5'].append(p5)
 
+    df_returns = pd.DataFrame.from_dict(returns)
+    df_returns.to_csv('***')
 
-df_returns = pd.DataFrame.from_dict(returns)
-df_returns.to_csv('****')
-
+end_time = dt.datetime.now()
+elapsed = end_time - start_time
+print('time total monte carlo', elapsed)
